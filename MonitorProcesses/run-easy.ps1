a9f5154346b43f1a78d32c95519087aae24cb2c3
kexuelou <cts-wzhao@live.com>
2015-02-10 12:03:39 +0800

    added an easier script
    just use get-process instead of restapi

---
+++ MonitorProcesses/run-easy.ps1
@@ -0,0 +1,13 @@
+﻿$sitename = $env:WEBSITE_SITE_NAME
+
+$time = Get-date -Format "yyyymmdd-hhmmss"
+$logFile = $env:HOME + "\LogFiles\" + $env:COMPUTERNAME + "-" + $sitename + "-" +  $time + ".log"
+
+
+Get-Process | Format-Table -Property  TotalProcessorTime, UserProcessorTime, CPU, HandleCount, PrivateMemorySize, NonpagedSystemMemorySize, PagedMemorySize, PeakPagedMemorySize, VirtualMemorySize,PeakVirtualMemorySize, WorkingSet, PeakWorkingSet | Out-File -Append $logFile
+
+
+ while (1 -eq 1){
+  Get-Process | Format-Table -HideTableHeaders  -Property ProcessName, Id, StartTime, TotalProcessorTime, UserProcessorTime, CPU, HandleCount, PrivateMemorySize, NonpagedSystemMemorySize, PagedMemorySize, PeakPagedMemorySize, VirtualMemorySize,PeakVirtualMemorySize, WorkingSet, PeakWorkingSet  | Out-File -Append $logFile
+  sleep -Seconds 1
+}
