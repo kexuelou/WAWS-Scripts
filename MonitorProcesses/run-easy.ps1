9021f5af4786079c3e66a6de35da583238b53cfc
kexuelou <cts-wzhao@live.com>
2015-02-11 15:25:49 +0800

    replaced format-table

--- MonitorProcesses/run-easy.ps1
+++ MonitorProcesses/run-easy.ps1
@@ -2,15 +2,10 @@
 $time = Get-date -Format "yyyymmdd-hhmmss"
 $logFile = $env:HOME + "\LogFiles\" + $env:COMPUTERNAME + "-" + $sitename + "-" +  $time + ".log"
 
-# | Format-Table -Property  TotalProcessorTime, UserProcessorTime, CPU, HandleCount, PrivateMemorySize, NonpagedSystemMemorySize, PagedMemorySize, PeakPagedMemorySize, VirtualMemorySize,PeakVirtualMemorySize, WorkingSet, PeakWorkingSet | Out-File -Append $logFile
-
 $content= "Time" + "`t" + "Start_Time" + "`t" + "ProcessName" + "`t" + "Id" + " " + "TotalProcessorTime" + "`t" + "UserProcessorTime"  + "`t" + "CPU"  + "`t" + "HandleCount"  + "`t" + "PrivateMemorySize"   + "`t" + "NonpagedSystemMemorySize"   + "`t" + "PagedMemorySize" + "`t" + "PeakPagedMemorySize" + "`t" + "VirtualMemorySize" + "`t" + "PeakVirtualMemorySize" + "`t" + "WorkingSet" + "`t" + "PeakWorkingSet"
 Add-Content $logFile $content
-#"Time`t`t`t`t`tName`t`t`tID`t`tstart_time`t`t`t`t`t`tthread_count`thandle_count`t`ttotal_cpu_time`t`tuser_cpu_time`t`tworking_set`t`tpeak_working_set`t`tprivate_memory`t`tvirtual_memory`t`tpeak_virtual_memory`t`tpaged_memory`t`tpeak_paged_memory`t`tpaged_system_memory`t`tnon_paged_system_memory"
 
  while (1 -eq 1){
-<<<<<<< HEAD
-   #Get-Process | Format-Table -HideTableHeaders  -Property ProcessName, Id, StartTime, TotalProcessorTime, UserProcessorTime, CPU, HandleCount, PrivateMemorySize, NonpagedSystemMemorySize, PagedMemorySize, PeakPagedMemorySize, VirtualMemorySize,PeakVirtualMemorySize, WorkingSet, PeakWorkingSet  | Out-File -Append $logFile
    $procs = Get-Process
    $time = Get-Date -Format "yyyy-MM-dd hh:mm:ss"
 
@@ -20,8 +15,4 @@ Add-Content $logFile $content
       Add-Content $logFile $content
    }
    sleep -Seconds 30
-=======
-  Get-Process | Format-Table -HideTableHeaders  -Property ProcessName, Id, StartTime, TotalProcessorTime, UserProcessorTime, CPU, HandleCount, PrivateMemorySize, NonpagedSystemMemorySize, PagedMemorySize, PeakPagedMemorySize, VirtualMemorySize,PeakVirtualMemorySize, WorkingSet, PeakWorkingSet  | Out-File -Append $logFile
-  sleep -Seconds 30
->>>>>>> origin/master
 }
